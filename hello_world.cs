f91324e124c5625f3567f3a9cbded860d89e14b4
PoplovVadim <poplovvadim@gmail.com>
2023-10-04 17:48:57 +0300

    updated file

--- hello_world.cs
+++ hello_world.cs
@@ -1,16 +1,13 @@
-
-
-using namespace std;
 namespace hello_world
 {
 class Program
+{
 static void Main()
 {
 Console.Write("Enter your name: ");
-string name = 
-Console.ReadLine();
-
+string name = Console.ReadLine();
 Console.WriteLine("Hello world from " + name);
+}
 }
 }
 
