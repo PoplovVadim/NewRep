9f29147f07881fc7156658166379c8cda2abdc9e
PoplovVadim <poplovvadim@gmail.com>
2023-10-04 18:21:16 +0300

    hello_world updated again

--- hello_world.cs
+++ hello_world.cs
@@ -4,9 +4,9 @@ class Program
 {
 static void Main()
 {
-Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("Hello world from " + name);
+Console.Write("Enter your name: "); //line output
+string name = Console.ReadLine(); //variable is being equated with entering data
+Console.WriteLine("Hello world from " + name); //output of the result
 }
 }
 }
