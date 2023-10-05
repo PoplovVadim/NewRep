28f6e594e74b38498370ec97b5f53167404dfe9b
stuuxxx <shipenkov.egor@yandex.ru>
2023-10-05 09:18:36 +0300

    new style

--- hello_world.cs
+++ hello_world.cs
@@ -1,13 +1,13 @@
 namespace hello_world
 {
 class Program
+{
 static void Main()
 {
 Console.Write("Enter your name: ");
-string name = 
-Console.ReadLine();
-
+string name = Console.ReadLine();
 Console.WriteLine("Hello world from " + name);
+}
 }
 }
 
